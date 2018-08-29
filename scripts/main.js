c32ee743602981831386d138ad74ab6161241420
Gunnar Headley <johngheadley@gmail.com>
2018-08-29 00:04:42 -0700

    Learning Javascript / Typescript. Picking color pallete.

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var myHeading = document.querySelector('#Head');
+myHeading.textContent = 'Hello world!';
+var test = 0;
+document.ondblclick = function () {
+    myHeading.textContent = (++test).toString();
+};
---
+++ scripts/main.ts
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('#Head');
+myHeading.textContent = 'Hello world!';
+
+var test = 0;
+
+document.ondblclick = function() {
+    myHeading.textContent = (++test).toString();
+};
