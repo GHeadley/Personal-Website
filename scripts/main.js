fbd7f547a4db1c57e7ddde40ca1d7add9fd84c51
Gunnar Headley <johngheadley@gmail.com>
2018-08-31 00:04:18 -0700

    Adding basic template of page on sites. Text is filler.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-var myHeading = document.querySelector('#Head');
-myHeading.textContent = 'Hello world!';
-var test = 0;
-document.ondblclick = function () {
-    myHeading.textContent = (++test).toString();
-};
+// var myHeading = document.querySelector('#Head');
+// myHeading.textContent = 'Hello world!';
+// var test = 0;
+// document.ondblclick = function() {
+//     myHeading.textContent = (++test).toString();
+// };
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,8 +1,8 @@
-var myHeading = document.querySelector('#Head');
-myHeading.textContent = 'Hello world!';
+// var myHeading = document.querySelector('#Head');
+// myHeading.textContent = 'Hello world!';
 
-var test = 0;
+// var test = 0;
 
-document.ondblclick = function() {
-    myHeading.textContent = (++test).toString();
-};
+// document.ondblclick = function() {
+//     myHeading.textContent = (++test).toString();
+// };
