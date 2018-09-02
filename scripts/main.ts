ab91c618f13ef842984ca732ba8833b4e0bb4069
Gunnar Headley <johngheadley@gmail.com>
2018-09-02 00:08:15 -0700

    Adding drop-down bar in menu.

--- scripts/main.ts
+++
@@ -1,8 +0,0 @@
-// var myHeading = document.querySelector('#Head');
-// myHeading.textContent = 'Hello world!';
-
-// var test = 0;
-
-// document.ondblclick = function() {
-//     myHeading.textContent = (++test).toString();
-// };
